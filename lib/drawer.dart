e5a0a731d79139e12c1e0d8d0ee4ca475211b375
kkoep <94103377+kkoep@users.noreply.github.com>
2022-12-11 18:32:58 +0700

    commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:c06_pas_pbp/main.dart';
 import 'package:c06_pas_pbp/page/wallofhope.dart';
-
-
+import 'package:c06_pas_pbp/page/article/article.dart';
 
 class PTS_Drawer extends StatelessWidget {
   const PTS_Drawer({super.key});
@@ -11,30 +10,39 @@ class PTS_Drawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
         child: Column(
-          children: [
-            // Menambahkan clickable menu
-            ListTile(
-              title: const Text('Main'),
-              onTap: () {
-                // Route menu ke halaman utama
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyHomePage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text('Wall of hope'),
-              onTap: () {
-                // Route menu ke halaman form
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const wallofhopePage()),
-                );
-              },
-            ),
-          ],
-        )
-      );
+      children: [
+        // Menambahkan clickable menu
+        ListTile(
+          title: const Text('Main'),
+          onTap: () {
+            // Route menu ke halaman utama
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage()),
+            );
+          },
+        ),
+        ListTile(
+          title: const Text('Wall of hope'),
+          onTap: () {
+            // Route menu ke halaman form
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const wallofhopePage()),
+            );
+          },
+        ),
+        ListTile(
+          title: const Text('Artikel Stunting'),
+          onTap: () {
+            // Route ke page artikel
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MyArticlePage()),
+            );
+          },
+        ),
+      ],
+    ));
   }
 }
