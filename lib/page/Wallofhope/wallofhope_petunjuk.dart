bd5bc45ae42ee5d860b02c640a208ede68371f19
Daffa Maulana Haekal <maulanadaffa342@gmail.com>
2022-12-12 19:11:20 +0700

    update 7:11 wib

--- lib/page/Wallofhope/wallofhope_petunjuk.dart
+++ lib/page/Wallofhope/wallofhope_petunjuk.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:c06_pas_pbp/page/wallofhope/wallofhope_menu.dart';
 import 'package:c06_pas_pbp/drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // ignore: camel_case_types
 class wallofhopePetunjukPage extends StatefulWidget {
   const wallofhopePetunjukPage({super.key});
 
-  final String title = 'Wall Of Hope';
+  final String title = 'Petunjuk Pembuatan Wall Of Hope';
 
   @override
   State<wallofhopePetunjukPage> createState() => _wallofhopePetunjukPage();
@@ -18,43 +19,40 @@ class _wallofhopePetunjukPage extends State<wallofhopePetunjukPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Petunjuk Pembuatan Wall of Hope'),
-        backgroundColor: Color(0xff395144),
+        title: Text(widget.title),
+        backgroundColor: Color(0xffAA8B56),
       ),
       drawer: const PTS_Drawer(),
-      body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  "https://user-images.githubusercontent.com/112499670/199527490-1c813ab3-6414-4b63-b8ad-b9e5c1cb8c8f.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: TextButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const wallofhopeMenuPage(),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Kembali',
-                    style: TextStyle(color: Colors.white),
-                  ),
+      body: Stack(children: [
+        Positioned.fill(
+          child:Image.asset('assets/Petunjuk.png', fit: BoxFit.cover),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
                 ),
-              )
-            ],
-          )),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const wallofhopeMenuPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Kembali',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        )
+      ]),
     );
   }
 }
