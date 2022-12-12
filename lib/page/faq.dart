73236fc465ec32930c213ae05f20ed9de61da547
navisraditya <navisraditya@gmail.com>
2022-12-12 14:09:34 +0700

    cepetan -yosua

---
+++ lib/page/faq.dart
@@ -0,0 +1,99 @@
+import 'package:c06_pas_pbp/page/faqdetailpage.dart';
+import 'package:flutter/material.dart';
+import 'package:c06_pas_pbp/model/faq_rec.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class FAQ extends StatefulWidget {
+  const FAQ({super.key});
+
+  @override
+  State<FAQ> createState() => _FAQstate();
+}
+
+class _FAQstate extends State<FAQ> {
+  Future<List<FaqRecommendations>> fetchFAQRec() async {
+    var url = Uri.parse('https://pts-c06-pbp.up.railway.app/faq/json/');
+    var response = await http.get(
+      url,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    );
+
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    List<FaqRecommendations> listFAQRec = [];
+    for (var d in data) {
+      if (d != null) {
+        listFAQRec.add(FaqRecommendations.fromJson(d));
+      }
+    }
+    return listFAQRec;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ListTile makeListTile(FaqRecommendations faqRec) => ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+          leading: Container(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: const Icon(Icons.autorenew, color: Colors.white),
+          ),
+          title: Text(
+            faqRec.fields.title,
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        FAQDetailPage(faqRecommendations: faqRec)));
+          },
+        );
+
+    Card makeCard(FaqRecommendations faqRec) => Card(
+          margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 1),
+          elevation: 4,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Container(
+            child: makeListTile(faqRec),
+          ),
+        );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Frequently Asked Questions"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+          future: fetchFAQRec(),
+          builder: (context, AsyncSnapshot snapshot) {
+            print(snapshot);
+            if (snapshot.hasData == null) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              if (!snapshot.hasData) {
+                return Column(
+                  children: const [
+                    Text(
+                      "Tidak ada FAQ",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (_, index) => makeCard(snapshot.data![index]));
+              }
+            }
+          }),
+    );
+  }
+}
---
+++ lib/page/faqdetailpage.dart
@@ -0,0 +1,55 @@
+import 'dart:html';
+
+import 'package:c06_pas_pbp/model/faq_rec.dart';
+import 'package:flutter/material.dart';
+
+class FAQDetailPage extends StatelessWidget {
+  final FaqRecommendations faqRecommendations;
+  const FAQDetailPage({super.key, required this.faqRecommendations});
+
+  Widget build(BuildContext context) {
+    final titleFAQ = Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          faqRecommendations.fields.title,
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ));
+    final descriptionFAQ = Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Row(
+        children: [
+          const Text(
+            "Jawaban: ",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          Text(faqRecommendations.fields.description,
+              style: const TextStyle(fontSize: 20)),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail'),
+      ),
+      body: Column(
+        children: [titleFAQ, descriptionFAQ],
+      ),
+      persistentFooterButtons: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            minimumSize: const Size.fromHeight(40),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text(
+            'Back',
+            style: TextStyle(fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+}
